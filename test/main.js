0bf68e7bf8191b8c7f567cd59f5834e2c07497af
david-rai <davidkhaling123@gmail.com>
2025-05-15 23:20:43 +0545

    Updated the readme.md

--- test/main.js
+++ test/main.js
@@ -11,18 +11,18 @@ const servers = {
 const initial = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     document.querySelector("main #myCamera").srcObject = stream
-
-    const remoteStream = new MediaStream()
-    document.querySelector("main #remoteCamera").srcObject = remoteStream
-
 }
 
+let peerConnection;
 //CREATING THE SDP OFFER
-const createOffer=async ()=>{
-    peerConnection=new RTCPeerConnection(servers)
+const createOffer = async () => {
+    peerConnection = new RTCPeerConnection(servers)
 
-    
     const remoteStream = new MediaStream()
     document.querySelector("main #remoteCamera").srcObject = remoteStream
+
+    //creating the offer
+    let offer=await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
 }
 initial()

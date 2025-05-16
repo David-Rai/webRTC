00c03451da605c7dbdb42a340824104f206b5261
david-rai <davidkhaling123@gmail.com>
2025-05-16 17:05:38 +0545

    Answer SDP created

--- test/main.js
+++ test/main.js
@@ -55,6 +55,42 @@ initial()
 
 
 //CREATING THE ANSWER
-const createAnswer=()=>{
-    alert("Creating the answer")
+const createAnswer=async ()=>{
+    console.log("creating the answer")
+    peerConnection = new RTCPeerConnection(servers)
+
+    //creating the stream for the remote user
+    remoteStream = new MediaStream()
+    document.querySelector("main #remoteCamera").srcObject = remoteStream
+
+//sending the track to the remote user
+stream.getTracks().forEach(track => {
+    peerConnection.addTrack(track, stream)
+})
+
+//getting  the remote track and showing into the webpage
+peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach(track => {
+        remoteStream.addTrack(track);
+    });
+};
+
+//event listeners for finding the ice candidate
+peerConnection.onicecandidate=async (event)=>{
+    if(event.candidate){
+document.querySelector("main .answer").innerText = JSON.stringify(peerConnection.localDescription)
+    }
+}
+
+//creating ths SDP answer
+let offer=document.querySelector("main .offer").value
+offer=await JSON.parse(offer)
+await peerConnection.setRemoteDescription(offer)
+
+//creating the answer SDP
+let answer=await peerConnection.createAnswer()
+await peerConnection.setLocalDescription(answer)
+
+document.querySelector("main .answer").value=JSON.stringify(answer)
+
 }

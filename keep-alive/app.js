5814cc3571d47ca37e9753e3b6b5774026bd8e99
david-rai <davidkhaling123@gmail.com>
2025-05-24 21:50:25 +0545

    Last commit of this repo

--- keep-alive/app.js
+++ keep-alive/app.js
@@ -1,16 +1,27 @@
-let counthere=document.querySelector("h3")
-let logs=document.querySelector("ul")
-let count=0
+let counthere = document.querySelector("h3")
+let logs = document.querySelector("ul")
+let count = 0
 
 async function activate() {
-    count+=1
-    counthere.innerText=count
+    count += 1
+
+    counthere.innerText = count
     const res = await fetch("https://webrtc-fvt3.onrender.com/")
-    logs.innerHTML +=`
-    <Li>${res.status}</li>
+    if (res.status === 200) {
+        logs.innerHTML += `
+    <Li style="backgroun-color:green;">${res.status}</li>
     `
+    } else {
+        logs.innerHTML += `
+    <Li style="backgroun-color:red;">${res.status}</li>
+    `
+    }
+
 }
 
 setInterval(() => {
     activate()
-},100)
+    if(count % 10 == 0){
+        logs.innerHTML=""
+    }
+}, 100)

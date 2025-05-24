3303f3a19aa7b267fd562892a3a9b4b55e41f39d
david-rai <davidkhaling123@gmail.com>
2025-05-24 21:34:05 +0545

    Funationality added

--- keep-alive/app.js
+++ keep-alive/app.js
@@ -1,6 +1,16 @@
-async function activate(){
-    console.log("fetching the data")
-    const res=await fetch("https://webrtc-fvt3.onrender.com/")
-    console.log(res)
+let counthere=document.querySelector("h3")
+let logs=document.querySelector("ul")
+let count=0
+
+async function activate() {
+    count+=1
+    counthere.innerText=count
+    const res = await fetch("https://webrtc-fvt3.onrender.com/")
+    logs.innerHTML +=`
+    <Li>${res.status}</li>
+    `
 }
-activate()
+
+setInterval(() => {
+    activate()
+}, 14 * 60 * 1000)
